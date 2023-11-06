4e89fea5901fa24017dab3d869e75676aafea53c
Physics722 <102660754+Physics722@users.noreply.github.com>
2023-11-06 16:24:56 +0800

    Update receiveSerial.py

--- receiveSerial.py
+++ receiveSerial.py
@@ -1,8 +1,23 @@
+import time
+import mido
+from mido import Message
+mido.set_backend('mido.backends.rtmidi')
+
+try:
+    outport  = mido.open_output('me', virtual='True')
+except:
+    print("Error: cannot connect to MIDI output")
+    
+inport = mido.open_input('Thomas', virtual='True')
+outport = mido.open_output('Thomas', virtual="True")
+
+print(outport)
+
 import serial
 
 # Configure the serial port
-numberPort = input("number port")
-ser = serial.Serial('/dev/ttyACM' + numberPort, 9600)  # Change '/dev/ttyUSB0' to the correct port
+#numberPort = input("number port")
+ser = serial.Serial('/dev/ttyACM0', 9600)  # Change '/dev/ttyUSB0' to the correct port
 #print('/dev/ttyACM' + numberPort)
 
 while True:
@@ -10,3 +25,6 @@ while True:
     data = ser.readline().decode('utf-8')
     print(data, end='')
     #do stuff with the data
+    msg = Message("pitchwheel", pitch = 8191)
+    outport.send(msg)
+
